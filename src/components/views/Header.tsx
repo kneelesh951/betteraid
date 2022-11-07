0947badca01b050b3d24d9ed616d1801cd1e57b6
unknown <kneelesh951@gmail.com>
2022-11-08 00:41:27 +0100

    initial files

---
+++ src/components/views/Header.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import styles from './Header.module.css';
+import {MetaData} from "../../_enonicAdapter/guillotine/getMetaData";
+import {getUrl} from "../../_enonicAdapter/UrlProcessor";
+
+export interface HeaderProps {
+    title: string;
+    logoUrl: string;
+    meta: MetaData;
+}
+
+
+const Header = ({title, logoUrl, meta}: HeaderProps) => {
+
+    return <header className={styles.header}>
+        <div className={styles.wrapper}>
+            {title && (
+                <h1>
+                    <a href={getUrl('', meta)}>{title}</a>
+                </h1>
+            )}
+            {logoUrl && (
+                <img src={logoUrl}
+                     width={33}
+                     height={40}
+                     alt={"Enonic XP logo"}
+                />
+            )}
+        </div>
+    </header>
+};
+
+export default Header
---
+++ src/components/views/Person.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import {FetchContentResult} from '../../_enonicAdapter/guillotine/fetchContent';
+import {getUrl} from "../../_enonicAdapter/UrlProcessor";
+
+const Person = (props: FetchContentResult) => {
+    const {displayName, data, parent} = props.data?.get as any;
+    const {bio, photos} = data;
+    const meta = props.meta;
+    const {_path} = parent;
+
+    return (
+        <>
+            <div>
+                <h2>{displayName}</h2>
+                <p>{bio}</p>
+                {
+                    photos.map((photo: any, i: number) => (
+                        <img key={i}
+                             src={photo.imageUrl}
+                             title={getTitle(photo, displayName)}
+                             alt={getTitle(photo, displayName)}
+                             width="500"
+                        />
+                    ))
+                }
+            </div>
+            <p><a href={getUrl(_path, meta)}>Back to Persons</a></p>
+        </>
+    )
+}
+
+export default Person;
+
+function getTitle(photo: any, displayName: string) {
+    return (photo.attachments || [])[0].name || displayName;
+}
